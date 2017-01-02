3d3233661dfaa29b0f36197d45edc5529c6c186f
Taylor Solomon <taylor@interactivestrategies.com>
2017-01-01 20:26:44 -0500

    Reading Plan

---
+++ sites/all/themes/portico/templates/portico-resources-bible-reading-plan-2017.tpl.php
@@ -0,0 +1,142 @@
+<?php
+
+  $uri  = 'public://';
+  $url = file_create_url($uri);
+
+?>
+
+<div class='resources'>
+
+  <h3>Phone Backgrounds</h3>
+
+  <div class='cards'>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Phone/2017BibleReadingPlan_PhoneBackground.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailPhone.jpg';?>" alt='Phone Background' />
+    </div>
+  </div>
+
+  <h3>Tablet Backgrounds</h3>
+
+  <div class='cards'>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Jan.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailJan.jpg';?>" alt='January' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Feb.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailFeb.jpg';?>" alt='February' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Mar.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailMar.jpg';?>" alt='March' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Apr.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailApr.jpg';?>" alt='April' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_May.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailMay.jpg';?>" alt='May' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Jun.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailJun.jpg';?>" alt='June' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Jul.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailJul.jpg';?>" alt='July' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Aug.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailAug.jpg';?>" alt='August' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Sep.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailSep.jpg';?>" alt='September' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Oct.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailOct.jpg';?>" alt='October' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Nov.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailNov.jpg';?>" alt='November' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Tablet/2017BibleReadingPlan_Tablet_Dec.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailDec.jpg';?>" alt='December' />
+    </div>
+  </div>
+
+  <h3>Desktop Backgrounds</h3>
+
+  <div class='cards'>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Jan.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailJan.jpg';?>" alt='January' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Feb.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailFeb.jpg';?>" alt='February' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Mar.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailMar.jpg';?>" alt='March' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Apr.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailApr.jpg';?>" alt='April' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_May.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailMay.jpg';?>" alt='May' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Jun.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailJun.jpg';?>" alt='June' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Jul.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailJul.jpg';?>" alt='July' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Aug.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailAug.jpg';?>" alt='August' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Sep.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailSep.jpg';?>" alt='September' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Oct.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailOct.jpg';?>" alt='October' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Nov.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailNov.jpg';?>" alt='November' />
+    </div>
+    <div class='resource-card'>
+      <a class='resource-card-link' target="_blank" href="<?php print $url . 'bible-reading-plan-2017/Desktop/2017BibleReadingPlan_Desktop_Dec.jpg';?>"></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailDec.jpg';?>" alt='December' />
+    </div>
+  </div>
+
+  <h3>Other Resources</h3>
+
+  <div class='cards'>
+    <div class='resource-card'>
+      <?php $href = $url . 'bible-reading-plan-2017/Bookmarks/2017BibleReadingPlan_Bookmarks.pdf'; ?>
+      <a class='resource-card-link' target="_blank" href="<?php print $href; ?>" ></a>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailBookmarks.jpg';?>" alt='December' />
+    </div>
+
+    <div class='resource-card'>
+      <?php $href = $url . 'bible-reading-plan-2017/Bookmarks/2017BibleReadingPlan.pdf'; ?>
+      <img src="<?php print $url . 'bible-reading-plan-2017/thumbnailEntire.jpg';?>" alt='December' />
+      <a class='resource-card-link' target="_blank" href="<?php print $href; ?>" ></a>
+    </div>
+  </div>
+
+
+</div>
