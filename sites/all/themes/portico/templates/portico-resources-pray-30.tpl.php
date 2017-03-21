a834e97dab44f3533654b8dad49806ed96fdcd61
Taylor Solomon <taylor@interactivestrategies.com>
2017-03-20 22:58:56 -0400

    Pushing Pray30 styles

--- sites/all/themes/portico/templates/portico-resources-pray-30.tpl.php
+++ sites/all/themes/portico/templates/portico-resources-pray-30.tpl.php
@@ -1,37 +1,102 @@
 <?php
 
-$uri  = 'public://';
-$url = file_create_url($uri);
-
-$num_images = 5;
-$prefix = 'Pray30_';
-
-$type_map = array(
-  array(
-    'type' => 'Phone',
-    'size' => '640x1136',
-  ),
-  array(
-    'type' => 'Tablet',
-    'size' => '1536x2048',
-  ),
-  array(
-    'type' => 'Desktop',
-    'size' => '5120x2880',
-  ),
-);
+  $uri  = 'public://';
+  $url = file_create_url($uri);
 
 ?>
+
 <div class='resources'>
-<?php foreach($type_map as $type): ?>
 
-  <h3><?php print $type['type']; ?></h3>
+  <h3>Phone Backgrounds</h3>
 
-  <?php foreach(range(1, $num_images) as $num): ?>
-    <?php $href = $url . 'pray-30/' . $prefix . $type['type'] . 'BG_' . $type['size'] . '_' . $num . '.jpg'; ?>
-    <a href="<?php print $href; ?>" target="_blank">$href</a>
-  <?php endforeach; ?>
+  <div class='notes'>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Phone/Pray30_PhoneBG_640x1136_Baby_Aug13.jpg';?>">
+        <span>Baby</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Phone/Pray30_PhoneBG_640x1136_DC_Aug13.jpg';?>">
+        <span>DC</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Phone/Pray30_PhoneBG_640x1136_Globe_Aug13.jpg';?>">
+        <span>Globe</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Phone/Pray30_PhoneBG_640x1136_Hands_Aug13.jpg';?>">
+        <span>Hands</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Phone/Pray30_PhoneBG_640x1136_Plain_Aug13.jpg';?>">
+        <span>Plain</span>
+      </a>
+    </div>
+  </div>
 
-<?php endforeach; ?>
+  <h3>Tablet Backgrounds</h3>
+
+  <div class='notes'>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Tablet/Pray30_TabletBG_1536x2048_Baby_Aug13.jpg';?>">
+        <span>Baby</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Tablet/Pray30_TabletBG_1536x2048_DC_Aug13.jpg';?>">
+        <span>DC</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Tablet/Pray30_TabletBG_1536x2048_Globe_Aug13.jpg';?>">
+        <span>Globe</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Tablet/Pray30_TabletBG_1536x2048_Hands_Aug13.jpg';?>">
+        <span>Hands</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Tablet/Pray30_TabletBG_1536x2048_Plain_Aug13.jpg';?>">
+        <span>Plain</span>
+      </a>
+    </div>
+  </div>
+
+
+  <h3>Desktop Backgrounds</h3>
+
+  <div class='notes'>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Desktop/Pray30_DesktopBG_5120x2880_Baby_Aug13.jpg';?>">
+        <span>Baby</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Desktop/Pray30_DesktopBG_5120x2880_DC_Aug13.jpg';?>">
+        <span>DC</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Desktop/Pray30_DesktopBG_5120x2880_Globe_Aug13.jpg';?>">
+        <span>Globe</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Desktop/Pray30_DesktopBG_5120x2880_Hands_Aug13.jpg';?>">
+        <span>Hands</span>
+      </a>
+    </div>
+    <div class='resource-note'>
+      <a class='button resource-note-link' target="_blank" href="<?php print $url . 'pray-30/Desktop/Pray30_DesktopBG_5120x2880_Plain_Aug13.jpg';?>">
+        <span>Plain</span>
+      </a>
+    </div>
+  </div>
+
 
 </div>
